480821216a3cf6704cff1e301b7a8495e199041e
Aun <aunh9277@gmail.com>
2026-07-06 18:34:08 +0100

    Created the user schema of my database

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const authRoute = require('./routes/authRoute');
 
 const app = express();
 const PORT = process.env.PORT;
+const connectionString = process.env.MONGO_CONNECTION_STRING
+
+mongoose.connect(connectionString)
+        .then(() => console.log('Connected to MongoDB'))
+        .catch((e) => {console.log(e.message)});
+
 
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.text());
+app.use('/auth', authRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port: ${PORT}`);
