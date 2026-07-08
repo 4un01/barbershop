db27a154e64cf667d0b489d6c980bc4ef58a6c96
Aun <aunh9277@gmail.com>
2026-07-08 16:04:24 +0100

    coded part of the function that loads days dynamically depending on the month and year

--- public/book.js
+++ public/book.js
@@ -1,12 +1,28 @@
 const days = document.querySelector('.days');
+const today = new Date();
+const monthAndYear = document.getElementById('monthAndYear');
+const months = [
+    'January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August',
+    'September', 'October', 'November', 'December' 
+];
 
-function test(){
-    for(let i =1; i <= 31; i++){
-        const day = document.createElement('p')
+function renderDays(date){
+    const month = date.getMonth();
+    const year = date.getFullYear();
+    const firstDay = new Date(year, month, 1).getDay();
+    const lastDay = new Date(year, month + 1, 0).getDate();
+
+    monthAndYear.textContent = `${months[month]} ${year}`;
+
+    for(let i = 1; i <= lastDay; i++){
+        const day = document.createElement('p');
         day.textContent = i;
         day.classList.add('day');
+        if(i === today.getDate() && month === today.getMonth() && year === today.getFullYear()){
+            day.classList.add('today');
+        } 
         days.appendChild(day);
-    }
+    };
 }
 
-test();
+renderDays(today);
