18b94578037ecdbe355d6557deff6b47793f6fea
Aun <aunh9277@gmail.com>
2026-07-08 16:43:08 +0100

    Coded the logic that ensures all dates match the days they fall on as well as added the previous months and the next months dates to any empty space

--- public/book.js
+++ public/book.js
@@ -6,15 +6,27 @@ const months = [
     'September', 'October', 'November', 'December' 
 ];
 
+
 function renderDays(date){
     const month = date.getMonth();
     const year = date.getFullYear();
     const firstDay = new Date(year, month, 1).getDay();
-    const lastDay = new Date(year, month + 1, 0).getDate();
+    const lastDate = new Date(year, month + 1, 0).getDate();
 
     monthAndYear.textContent = `${months[month]} ${year}`;
 
-    for(let i = 1; i <= lastDay; i++){
+    const lastDatePrevMonth = new Date(year, month, 0).getDate();
+    const lastDay = new Date(year, month + 1, 0).getDay();
+    console.log(lastDay);
+    
+    for(let j = firstDay; j > 0; j--){
+        const daysBefore = document.createElement('p');
+        daysBefore.textContent = lastDatePrevMonth - j;
+        daysBefore.classList.add('daysBefore');
+        days.appendChild(daysBefore);
+    };
+
+    for(let i = 1; i <= lastDate; i++){
         const day = document.createElement('p');
         day.textContent = i;
         day.classList.add('day');
@@ -23,6 +35,17 @@ function renderDays(date){
         } 
         days.appendChild(day);
     };
+
+    let nextMonthsDateCount = 0;
+    for(let k = lastDay; k < 6; k++){
+        nextMonthsDateCount++;
+        const daysAfter = document.createElement('p');
+        daysAfter.textContent = nextMonthsDateCount;
+        daysAfter.classList.add('daysAfter');
+        days.appendChild(daysAfter);
+    }
+
+
 }
 
 renderDays(today);
